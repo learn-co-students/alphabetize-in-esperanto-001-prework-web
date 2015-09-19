32e4492107274fd90d601c43c83791804c141f48
pedrotp <pedro@pedrotp.com>
2015-09-19 13:37:35 -0700

    ordo kaj aranĝis

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alpha_array = alphabet.split(//)
+  sorted_arr = arr.sort_by do |word| 
+    [alpha_array.index(word[0]),
+    alpha_array.index(word[1]),
+    alpha_array.index(word[2]),
+    alpha_array.index(word[3]),
+    alpha_array.index(word[4]),
+    alpha_array.index(word[5]),
+    alpha_array.index(word[6])]
+  end
+  sorted_arr
 end
+
+alphabetize(["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"])
