c8d4c76e29b3c60623b5393cf7314cec3aebb3f1
chenghw <ho.won.cheng@gmail.com>
2015-09-25 22:17:46 -0400

    esperanto complete

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  esperanto_num_arr = []
+  arr.each do |word|
+    temp_arr = []
+    word.split('').each do |letter|
+      temp_arr << esperanto_alphabet.split('').index(letter)
+    end
+    esperanto_num_arr << temp_arr
+  end
+
+  esperanto_num_arr.sort_by! do |num_arr|
+    num_arr
+  end
+
+  sorted_arr = []
+  esperanto_num_arr.each do |word|
+    temp_arr = []
+    word.each do |letter|
+      if letter == nil
+        temp_arr << ' '
+      else
+        temp_arr << esperanto_alphabet.split('')[letter]
+      end
+    end
+    sorted_arr << temp_arr.join('')
+  end
+
+  sorted_arr
 end
