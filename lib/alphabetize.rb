0439ac657a558a13072cde38c0bdb1303823fae6
hwinn4 <hilary.winn@gmail.com>
2015-09-27 17:04:22 -0400

    old version

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto_alphabet = esperanto_alphabet.split("")
+
+  esperanto_hash = {}
+  esperanto_alphabet.each_with_index do |letter, index|
+    esperanto_hash[letter] = index
+  end
+
+
+  sorted_list = arr.sort_by do |word|
+    esperanto_hash.index(word)
+  end
+
+
+  #binding.pry
+  sorted_list
 end
