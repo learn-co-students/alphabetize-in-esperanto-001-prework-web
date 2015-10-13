19aeee983b23a2781fb6a9d1fba47422dc5875a0
dunxtand <mdrstandish@gmail.com>
2015-10-13 17:50:11 -0400

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  index = 0
+  loop do
+    letter = arr[0][index]
+    if arr.all? { |phrase| phrase[index] == letter }
+      index += 1
+    else
+      break
+    end
+  end
+  arr.sort_by do |phrase|
+    alphabet.find_index { |letter| letter == phrase[index] }
+  end
 end
