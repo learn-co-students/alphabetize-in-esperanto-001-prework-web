37c4acbd7bbb4f732865a19ec21549e8a4e8886c
dougtebay <dougtebay@gmail.com>
2016-01-15 16:01:09 -0600

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    index_arr = []
+    for i in 0.upto(phrase.split.join.length - 1) do
+      index_arr << alphabet.index(phrase.split.join[i])
+    end
+    index_arr
+  end
 end
