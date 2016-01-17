2adb28975f7ea38dbf7aaf8f329630261ebf44e6
HarlemSquirrel <wittyman37@yahoo.com>
2016-01-17 18:30:27 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  sorted_array = arr.sort
+  sorted_array = sorted_array.sort_by { |a|
+    ESPERANTO_ALPHABET.index(a[0])
+  }
 end
