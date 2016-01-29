63c18d4ee19fe1df86af43e3df7d344cfd378a2b
fossbender <fossbender@gmail.com>
2016-01-29 16:19:10 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
-def alphabetize(arr)
-  # code here
+  require 'pry'
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
+  def alphabetize(to_sort)
+
+    custom_sort = ->(a, b) do
+      a.split('').each_with_index do |char, i|
+        return ESPERANTO_ALPHABET.index(a[i]) <=> ESPERANTO_ALPHABET.index(b[i]) if a[i] != b[i]
+      end
+      return ESPERANTO_ALPHABET.index(' ') <=> ESPERANTO_ALPHABET.index(b[-1])
+    end
+
+
+    to_sort.sort(&custom_sort)
+
 end
