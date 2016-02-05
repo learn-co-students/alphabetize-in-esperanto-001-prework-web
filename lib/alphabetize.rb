ab3459a79e11b49cbcac55f3dc4c2c28239e41de
aback1227 <aback1227@gmail.com>
2016-02-05 18:37:53 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  arr.sort_by do |word| 
+    word.split('').map { |letter| esperanto_array.index(letter) }
+  end
 end
