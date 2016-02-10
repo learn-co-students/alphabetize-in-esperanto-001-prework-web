3dbba23df5a35f565240f08e24782a109e2690f4
WilliamAG <william.a.gough@gmail.com>
2016-02-10 00:20:52 -0500

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
-def alphabetize(arr)
+ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def compare(x, y)
+  # x <=> y
+  n = [x.length, y.length].min - 1
+  (0..n).each do |i|
+    if ALPHABET.index(x[i]) < ALPHABET.index(y[i])
+      return -1
+    elsif ALPHABET.index(x[i]) > ALPHABET.index(y[i])
+      return 1
+    end
+  end
+end
+
+
+def alphabetize(array)
+  array.sort do |x,y| 
+    compare(x,y)
+  end   
+
   # code here
 end
