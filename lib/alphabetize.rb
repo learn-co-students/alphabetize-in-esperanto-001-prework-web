decdd14ff81cfcff4916eccb99e28f9ffe3f3c19
Brahimndaw <brahimndaw@gmail.com>
2016-03-08 18:49:08 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
-def alphabetize(arr)
-  # code here
-end
+ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ 
+  def alphabetize(arr)
+   # code here
+   arr.sort_by! do |phrase|
+     phrase.tr(ESPERANTO_ALPHABET, 'a-z')
+   end
+  end  
