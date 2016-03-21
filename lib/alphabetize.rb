92b0d0b1f0c980c104f566f402a6a3e97aba1e79
bradtemple <brad.temple@gmail.com>
2016-03-20 23:31:51 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
 def alphabetize(arr)
-  # code here
+	alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+    arr.sort_by! do |x|
+  		word_index = x.delete(" ").chars
+  		index_values = []
+  		word_index.each do |y|
+  			points = alphabet.index(y)
+  			index_values << points
+  		end
+  		index_values
+  	end
+  	arr
 end
