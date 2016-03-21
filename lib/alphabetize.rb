e94baddc58ac24b2d1acad6f1ee4d98364e2cbf4
steve21168 <steve21168@gmail.com>
2016-03-21 13:43:40 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabet_hash
+  alpha_hash = {}
+
+  ALPHABET.each_char.with_index do |letter, idx|
+    alpha_hash[letter] = idx
+  end
+  
+  alpha_hash
+end
+
 def alphabetize(arr)
-  # code here
+  new_words = nil
+
+  arr.sort_by do |word|
+    word.each_char.map do |letter|
+      alphabet_hash[letter]
+    end
+  end
 end
