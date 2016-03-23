1670e13aa124f98ea6380f95397f59707ce3ffb8
davidtsai130 <davidtsai130@gmail.com>
2016-03-23 06:06:56 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,9 @@
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  
-  
+
+  arr.sort_by do |a|
+    a.split('').collect do |letter|
+      alphabet.index(letter)
+    end
+  end
 end
