ceed65ddc2db3b20c9b1dc89b670f0b8f35db41d
jonalexander <jonathandalexander@gmail.com>
2016-03-23 14:23:40 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+	esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  newArr = arr.sort_by do |word|
+  	word.split('').map do |char|
+  		esperanto.index(char)
+  	end
+  end
 end
+
+
+#sort_by individual characters and their index positions within
+#the esperanto array
+#modify the block
