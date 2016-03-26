cf45e4bacb0df8d89186b86406aa101e377e86bd
alemosie <alexandra.siega@gmail.com>
2016-03-26 17:17:18 -0400

    alphabet into hash, sort_by chars in array phrases against hash

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  # ALPH index of each letter in hash, e.g. a = 0, ĉ = 3
+  alphabetHash = alphabet.each_char.with_index.to_a.to_h
+
+  arr.sort_by do |phrase|
+    phrase.chars.map {|char| alphabetHash[char] }
+  end
 end
