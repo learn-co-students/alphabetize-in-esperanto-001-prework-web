8fdc4bea24f088027a1061e22122b7f8ab15f47f
Tal <talgoldfus@gmail.com>
2016-03-27 05:26:26 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,10 +2,9 @@ require 'pry'
 
 def alphabetize(arr)
 alphabet = 'abcĉdefgĝhĥijĵklmnoprsŝtuŭvz'
-ralphabet='abcdefghijklmnopqrstuvwxyz'
-  
-arr.sort_by{|string| string.tr(alphabet, ralphabet)}
+ralphabet= 'abCcdefGgHhiJjklmnoprsStuUvz'
 
+arr.sort_by{|string| string.tr(alphabet,ralphabet)}
  end
 
 
@@ -43,8 +42,7 @@ finalarr<<finalwords.join(" ")
     end
 finalarr.join(" ")
  end
-#mi amas vin
+
 #{"a"=>0, "b"=>1, "c"=>2, "ĉ"=>3, "d"=>4, "e"=>5, "f"=>6, "g"=>7, "ĝ"=>8, "h"=>9, "ĥ"=>10, "i"=>11, "j"=>12, "ĵ"=>13, "k"=>14, "l"=>15, "m"=>16, "n"=>17, "o"=>18, "p"=>19, "r"=>20, "s"=>21, "ŝ"=>22, "t"=>23, "u"=>24, "ŭ"=>25, "v"=>26, "z"=>27}
 
-#finalarr
 
