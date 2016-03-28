4e729ef281e895fe75db412626bde6747799a966
kdonovan2005 <kdonovan2005@gmail.com>
2016-03-28 19:44:15 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  alphahash = alphabet.each_char.with_index.to_h
+
+  arr.sort_by do |phrase|
+    phrase.chars.collect {|char| alphahash[char]}
+  end
 end
