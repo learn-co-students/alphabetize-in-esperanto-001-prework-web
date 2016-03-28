f8a3be7875c9c7fc26eeb6fb417585b2e25945f6
wengong <wentoshare@gmail.com>
2016-03-28 01:36:13 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
 def alphabetize(arr)
   # code here
+  abc = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
+  arr.sort_by do |i|
+    i.chars.map { |g| abc.chars.index(g) }
+  end
 end
+
+
+
+=begin
+def alphabetize(phrases)
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  phrases.sort_by do |phrase|
+    phrase.chars.map { |c| alpha.index(c) }
+  end
+end
+=end
