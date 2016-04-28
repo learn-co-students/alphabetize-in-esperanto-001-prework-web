2f6951a9f7332b1f508bf4e11143b9b367adedcd
thedailyephemera <thedailyephemera@gmail.com>
2016-04-28 17:02:32 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
-def alphabetize(arr)
-  # code here
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(arr) 
+	return arr.sort_by! {|excerpt| excerpt.tr(ESPERANTO_ALPHABET, '1-2a-z')}
 end
+
+alphabetize(["bonan matenon", "bonan nokton", "bonan vesperon"])
+puts "\n\n"
+alphabetize(["mi amas vin", "ĉu vi parolas esperanton", "bonan matenon", "pacon"])
