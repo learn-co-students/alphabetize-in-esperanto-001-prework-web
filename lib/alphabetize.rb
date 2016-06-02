3f56384e3ee262edc65441189bc480b44c9e6cd0
colbyschulz <colbyschulz@gmail.com>
2016-06-02 16:38:05 -0500

    alphabetastic

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto_array = esperanto.chars
+  arr.sort_by do |i|
+    i.chars.collect do |char|
+      esperanto_array.index(char)
+    end
+  end
 end
