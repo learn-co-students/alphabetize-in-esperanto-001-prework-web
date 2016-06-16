86dbe193845fed1dc086236b3158dc604a8029fe
Gentian Bardhoshi <gbardhoshI@gmail.com>
2016-06-16 04:41:24 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,12 +4,6 @@ def alphabetize(arr)
 
   esp_hash = esperanto_alphabet.zip(esp_alphabet_index).to_h
 
-  # {"a"=>1, "b"=>2, "c"=>3, "ĉ"=>4, "d"=>5, "e"=>6, "f"=>7,
-  # "g"=>8, "ĝ"=>9, "h"=>10, "ĥ"=>11, "i"=>12, "j"=>13,
-  # "ĵ"=>14, "k"=>15, "l"=>16, "m"=>17, "n"=>18, "o"=>19,
-  # "p"=>20, "r"=>21, "s"=>22, "ŝ"=>23, "t"=>24, "u"=>25,
-  # "ŭ"=>26, "v"=>27, "z"=>28}
-
   arr.sort do |a_string,b_string|
     i = 0
     while esp_hash[a_string[i]] == esp_hash[b_string[i]]
