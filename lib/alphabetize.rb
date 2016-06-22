b537f24eda5860538d8bc1ce2cea3c50076a0449
jinstrider2000 <efrainperezjr@live.com>
2016-06-22 17:39:40 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  esperanto_hash = {}
+  ALPHABET.chars.each_with_index {|letter,index| esperanto_hash[letter] = index+1 }
+  arr.sort_by do |word_or_phrase|
+    array_representation = word_or_phrase.chars
+    array_representation.delete(" ")
+    array_representation.map { |letter| esperanto_hash[letter] }
+  end
 end
