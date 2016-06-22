bf7d35e7698a0db5615a1b5a6595b676cfd72d39
alispace <alispace410@gmail.com>
2016-06-22 01:00:36 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,24 @@
 def alphabetize(arr)
   # code here
+  new_array = []
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  # sort through the phrases
+  new_array = arr.sort_by do |i|
+    # remove all of the spaces from each string
+    no_spaces = i.delete(" ")
+
+    # convert the string to an index of characters
+    char_array = no_spaces.split(//)
+    num_array = []
+
+    # create an array of their positions in the alphabet for each character
+    char_array.each do |char|
+      ind = alphabet.index(char)
+      num_array << ind
+    end
+
+    num_array
+  end
+  new_array
 end
