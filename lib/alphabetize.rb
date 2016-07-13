1008b6e772009cc9fdf8dcec9a62853993b159fe
adam2222 <adam222@comcast.net>
2016-07-13 12:19:13 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,17 +1,16 @@
 def alphabetize(array)
-  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  esperanto_alphabet = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
 
   array.sort_by do |word|
-    [
-      esperanto_alphabet.index(word[0]), 
-      esperanto_alphabet.index(word[1]),
-      esperanto_alphabet.index(word[2]),
-      esperanto_alphabet.index(word[3]),
-      esperanto_alphabet.index(word[4]),
-      esperanto_alphabet.index(word[5]),
-      esperanto_alphabet.index(word[6])
-    ]
+    i = 0
+    sort_by_array = []
+
+    word.length.times do
+      sort_by_array << esperanto_alphabet.index(word[i])
+      i += 1
+    end
     
+    sort_by_array
   end
 
 end
