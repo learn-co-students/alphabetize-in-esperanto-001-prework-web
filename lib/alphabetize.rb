f1b04a4c2ad2bd87c10df07487569f6462a42001
OliviaGunton <olivia.gunton@gmail.com>
2016-08-06 14:27:31 -0400

    Alphabetize array of phrases using custom alphabet order

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,23 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+#Build hash of alphabet characters so lookup is O(1)
+def build_character_hash(alphabet_string)
+  character_hash = {}
+
+  alphabet_string.each_char.with_index do |char, index|
+    character_hash[char] = index
+  end
+
+  character_hash
+end
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet_hash = build_character_hash(ESPERANTO_ALPHABET)
+
+  arr.sort_by do |phrase|
+    phrase.chars.map do |c|
+      esperanto_alphabet_hash[c]
+    end
+  end
+
 end
