1a38d7cdf1b0fa1c379b4ca38979f6ef7f502a62
npatel111 <nitimaheshpatel@gmail.com>
2016-08-19 15:22:57 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
 def alphabetize(arr)
   # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by { |phrase| phrase.chars.collect {|c| esperanto.index(c)}}
 end
+
+#I found this code on stack overflow: http://stackoverflow.com/questions/28368857/how-to-sort-an-array-of-accented-words-in-ruby
